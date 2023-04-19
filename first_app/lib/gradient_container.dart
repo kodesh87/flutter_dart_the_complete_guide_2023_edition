54a9e080cf0267b50a86e87c7d1868c2d76f0836
Wira Nov Kurnia Sihombing <wira.sdamm@gmail.com>
2023-04-19 16:43:21 +0700

    Lesson #44

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.gradientColors});
+  const GradientContainer.redToBlack({super.key})
+      : gradientColors = const [Colors.red, Colors.black];
   final List<Color> gradientColors;
 
   @override
@@ -18,7 +19,13 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText("Hello World 2")),
+      child: Center(
+        child: Image.asset(
+          "assets/images/dice/dice-2.png",
+          width: 200,
+        ),
+      ),
+      // child: Image.asset("name"),
     );
   }
 }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:first_app/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      // backgroundColor: Color.fromARGB(255, 134, 176, 136),
-      body: GradientContainer(
-        gradientColors: [
-          Color.fromARGB(255, 60, 117, 63),
-          Color.fromARGB(255, 82, 44, 208),
-        ],
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Color.fromARGB(255, 134, 176, 136),
+        body: GradientContainer.redToBlack(),
+        // body: GradientContainer(
+        //   gradientColors: [
+        //     Color.fromARGB(255, 60, 117, 63),
+        //     Color.fromARGB(255, 82, 44, 208),
+        //   ],
       ),
     ),
-  ));
+  );
 }
