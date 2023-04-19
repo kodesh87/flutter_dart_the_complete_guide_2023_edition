9105b00accc066f0278791e1ef0fad26c9ef5183
Wira Nov Kurnia Sihombing <wira.sdamm@gmail.com>
2023-04-19 14:06:51 +0700

    Lesson #39

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 134, 176, 136),
             Color.fromARGB(255, 228, 83, 39),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(child: StyledText()),
