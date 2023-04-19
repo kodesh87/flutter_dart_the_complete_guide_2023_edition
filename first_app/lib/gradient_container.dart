a3146eaf6778a4d18c016bba72abbc871fcff9ff
Wira Nov Kurnia Sihombing <wira.sdamm@gmail.com>
2023-04-19 14:01:42 +0700

    Lesson #38

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -16,15 +17,7 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: Text(
-          "Hello World",
-          style: TextStyle(
-            fontSize: 28,
-            color: Colors.white,
-          ),
-        ),
-      ),
+      child: const Center(child: StyledText()),
     );
   }
 }
