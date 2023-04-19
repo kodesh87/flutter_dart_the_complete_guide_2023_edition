bc49a1c96ff3748a4b1068ce7e6019624915d8fb
Wira Nov Kurnia Sihombing <wira.sdamm@gmail.com>
2023-04-19 17:03:49 +0700

    Lesson #46

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -25,14 +25,34 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               "assets/images/dice/dice-2.png",
               width: 200,
             ),
-            OutlinedButton(onPressed: () {}, child: const Text("Roll Dice")),
-            const ElevatedButton(onPressed: rollDice, child: Text("Roll Dice")),
-            TextButton(onPressed: () {}, child: const Text("Roll Dice"))
+            SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text("Roll Dice"),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Roll Dice"),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text("Roll Dice"),
+            )
           ],
         ),
       ),
